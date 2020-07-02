74cb6d9db128a1699053f6dfa7d6557f88639e10
Jakob <jakob@folkdatorn.se>
2020-07-02 23:09:48 +0200

    First build

---
+++ docs/precache-manifest.adadc4fc3cbe062edbd9cfa5a323701e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a639b4f5e8837e573a48adc813d3d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066ebc07394bd89051a8",
+    "url": "/static/css/main.6b21649b.chunk.css"
+  },
+  {
+    "revision": "17d0c4f6b0e3230c626c",
+    "url": "/static/js/2.1c260e7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c260e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066ebc07394bd89051a8",
+    "url": "/static/js/main.4b175aac.chunk.js"
+  },
+  {
+    "revision": "202ebef69ca430576136",
+    "url": "/static/js/runtime-main.d71cd019.js"
+  }
+]);
